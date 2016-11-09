914c3a43677a8a46dcfb02c267954220ee41fa81
dongjokim <dong.jo.kim@cern.ch>
2016-11-09 11:14:38 +0100

    changed the place of figures

--- makediff.sh
+++ makediff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 #1. version to PC 7f420a7
 #2. merge sec to one file 2cfb30f
-../git-latexdiff/git-latexdiff --main paper.tex 7f420a7 $1
+../git-latexdiff/git-latexdiff --main paper.tex d0c5866 a53b9e1
