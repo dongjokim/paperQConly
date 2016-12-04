d104ae064b3fd4d6ba75663c51471ad86de9420a
dongjokim <dong.jo.kim@cern.ch>
2016-12-05 00:22:04 +0200

    final version to IRC R2

--- makediff.sh
+++ makediff.sh
@@ -2,4 +2,5 @@
 #1. version to PC 7f420a7
 #2. merge sec to one file 2cfb30f
 #3. IRC first round : 7f129af
-../git-latexdiff/git-latexdiff --main paper.tex 7f129af $1
+#../git-latexdiff/git-latexdiff --main paper.tex 7f129af $1
+../git-latexdiff/git-latexdiff --main paper.tex master IRCR1
